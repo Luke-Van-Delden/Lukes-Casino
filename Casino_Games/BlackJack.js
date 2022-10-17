ae7f7b8e112a7343e1a1e050cffe4f3a98d700a1
Luke Van Delden <vandelden.luke@gmail.com>
2022-10-17 16:14:08 -0500

    added prompt to play and loop to play

--- Casino_Games/BlackJack.js
+++ Casino_Games/BlackJack.js
@@ -3,62 +3,61 @@
 //  Meant to signify Ace, 2, 3... etc Jack, Queen, King
 //  console.log((Math.floor(Math.random() * 13)) + 1);
 
+let playGame = confirm("Would you like to play Blackjack?")
 
-
-let playerHand = newHand();
-let playerAction = "";
-let dealerHand;
+while (playGame == true) {
+    let playerHand = newHand();
+    let playerAction = "";
+    let dealerHand;
 // PLAYER SIDE TO HIT STAY OR BUST
-do{
-    playerAction = prompt("Type 'hit' to draw, 'stay' to stay.")
-    if (playerAction == 'hit'){
-        let newcard = draw()
-        alert("You've drawn a " + cardType(newcard));
-        playerHand = playerHand + cardValue(newcard);
-        alert("Your new total is: " + playerHand);
-        if (playerHand > 21){
-            alert("Player has busted");
-        }
-    }
-    else if (playerAction == 'stay') {
-        alert("Player stays on " + playerHand);
-        break;
-    }
-} while (playerHand <=21 )
-
-if (playerHand < 22) {
-    let dealerHand = dealerNew();
     do {
-        if (dealerHand > 17 && dealerHand <= 21) {
-            alert("Dealer stays on " + dealerHand);
-            break;
-        } else if (dealerHand > 21) {
-            alert("Dealer busts")
-            break;
-        } else if (dealerHand < 17) {
+        playerAction = prompt("Type 'hit' to draw, 'stay' to stay.")
+        if (playerAction == 'hit') {
             let newcard = draw()
-            alert("Dealer draws a " + cardType(newcard));
-            dealerHand = dealerHand + cardValue(newcard);
-            alert("Dealer's new total is: " + dealerHand);
+            alert("You've drawn a " + cardType(newcard));
+            playerHand = playerHand + cardValue(newcard);
+            alert("Your new total is: " + playerHand);
+            if (playerHand > 21) {
+                alert("Player has busted");
+            }
+        } else if (playerAction == 'stay') {
+            alert("Player stays on " + playerHand);
+            break;
         }
-    } while (dealerHand !== true)
+    } while (playerHand <= 21)
 
-    console.log(dealerHand + " " + playerHand)
-}
-if (playerHand > dealerHand && playerHand <= 21) {
-    alert("Players " + playerHand + " beats dealers " + dealerHand);
-    alert("Player wins lotsa money");
-}
-else if (playerHand <= 21 && dealerHand > 21) {
-    alert("Dealer busted, winner winner chicken dinner!")
-}
-else if (playerHand == dealerHand){
-    alert("Its a push. Both dealer and player have " + playerHand);
-}
-else {
-    alert("Better luck next time!");
-}
+    if (playerHand < 22) {
+        let dealerHand = dealerNew();
+        do {
+            if (dealerHand > 17 && dealerHand <= 21) {
+                alert("Dealer stays on " + dealerHand);
+                break;
+            } else if (dealerHand > 21) {
+                alert("Dealer busts")
+                break;
+            } else if (dealerHand < 17) {
+                let newcard = draw()
+                alert("Dealer draws a " + cardType(newcard));
+                dealerHand = dealerHand + cardValue(newcard);
+                alert("Dealer's new total is: " + dealerHand);
+            }
+        } while (dealerHand !== true)
 
+        console.log(dealerHand + " " + playerHand)
+    }
+    if (playerHand > dealerHand && playerHand <= 21) {
+        alert("Players " + playerHand + " beats dealers " + dealerHand);
+        alert("Player wins lotsa money");
+    } else if (playerHand <= 21 && dealerHand > 21) {
+        alert("Dealer busted, winner winner chicken dinner!")
+    } else if (playerHand == dealerHand) {
+        alert("Its a push. Both dealer and player have " + playerHand);
+    } else {
+        alert("Better luck next time!");
+    }
+    playGame = confirm("Would you like to play Blackjack?")
+
+}
 
 function draw (){
     return ((Math.floor(Math.random() * 13)) + 1);
