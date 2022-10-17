15d5013b517c2d0555bad711aaa8a5ddc69d9b5f
Luke Van Delden <vandelden.luke@gmail.com>
2022-10-17 16:06:25 -0500

    lotsa progress and refining. Added dealer

--- Casino_Games/BlackJack.js
+++ Casino_Games/BlackJack.js
@@ -7,7 +7,8 @@
 
 let playerHand = newHand();
 let playerAction = "";
-
+let dealerHand;
+// PLAYER SIDE TO HIT STAY OR BUST
 do{
     playerAction = prompt("Type 'hit' to draw, 'stay' to stay.")
     if (playerAction == 'hit'){
@@ -15,6 +16,9 @@ do{
         alert("You've drawn a " + cardType(newcard));
         playerHand = playerHand + cardValue(newcard);
         alert("Your new total is: " + playerHand);
+        if (playerHand > 21){
+            alert("Player has busted");
+        }
     }
     else if (playerAction == 'stay') {
         alert("Player stays on " + playerHand);
@@ -22,9 +26,39 @@ do{
     }
 } while (playerHand <=21 )
 
-if (playerHand > 21) {
-    alert("Player had busted. Better luck next time!");
+if (playerHand < 22) {
+    let dealerHand = dealerNew();
+    do {
+        if (dealerHand > 17 && dealerHand <= 21) {
+            alert("Dealer stays on " + dealerHand);
+            break;
+        } else if (dealerHand > 21) {
+            alert("Dealer busts")
+            break;
+        } else if (dealerHand < 17) {
+            let newcard = draw()
+            alert("Dealer draws a " + cardType(newcard));
+            dealerHand = dealerHand + cardValue(newcard);
+            alert("Dealer's new total is: " + dealerHand);
+        }
+    } while (dealerHand !== true)
+
+    console.log(dealerHand + " " + playerHand)
 }
+if (playerHand > dealerHand && playerHand <= 21) {
+    alert("Players " + playerHand + " beats dealers " + dealerHand);
+    alert("Player wins lotsa money");
+}
+else if (playerHand <= 21 && dealerHand > 21) {
+    alert("Dealer busted, winner winner chicken dinner!")
+}
+else if (playerHand == dealerHand){
+    alert("Its a push. Both dealer and player have " + playerHand);
+}
+else {
+    alert("Better luck next time!");
+}
+
 
 function draw (){
     return ((Math.floor(Math.random() * 13)) + 1);
@@ -132,3 +166,16 @@ function newHand (){
 
 }
 
+function dealerNew (){
+    let firstCard = draw();
+    let secondCard = draw();
+
+    let total = 0;
+    total = cardValue(firstCard) + cardValue(secondCard);
+
+    alert("Dealer's hand: " + cardType(firstCard) + " & " + cardType(secondCard));
+
+    alert("Dealer's hand current total: " + total);
+    return total;
+
+}
