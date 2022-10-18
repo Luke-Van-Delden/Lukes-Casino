a33030d6f9bf80448f878fcaaac637f37acd9473
Luke Van Delden <vandelden.luke@gmail.com>
2022-10-17 22:03:26 -0500

    added wishlist, fixed Winning/Losing outcome if statements

--- Casino_Games/BlackJack.js
+++ Casino_Games/BlackJack.js
@@ -1,61 +1,66 @@
+// // Ideas to Workon
+// Making Aces 1 or 11
+// Identifying suit of card
+// Setting number of decks
+// Betting / Maintaining chip counts
+// Visuals / buttons / interactive
+// Tips or "What does the book say?"
+// Double Down / Split options
+// Multiple Hands
+
 
 //  Generates random number 1-13
 //  Meant to signify Ace, 2, 3... etc Jack, Queen, King
 //  console.log((Math.floor(Math.random() * 13)) + 1);
 
 let playGame = confirm("Would you like to play Blackjack?")
-let dealerHand;
 
 while (playGame == true) {
     let playerHand = newHand();
+    let dealerHand = dealerNew();
     let playerAction = "";
 // PLAYER SIDE TO HIT STAY OR BUST
     do {
-        playerAction = prompt("Type 'hit' to draw, 'stay' to stay.")
+        playerAction = prompt("Type 'hit' to draw, 'stay' to stay." + "\nPlayer hand: " + playerHand)
 
         if (playerAction == 'hit') {
-            let newcard = draw()
-            alert("You've drawn a " + cardType(newcard));
+            let newcard = draw();
             playerHand = playerHand + cardValue(newcard);
-            alert("Your new total is: " + playerHand);
+            alert("You've drawn a " + cardType(newcard) + "\n Your hand: " + playerHand);
             if (playerHand > 21) {
-                alert("Player has busted");
+                alert("Player has busted with " + playerHand);
             }
-        } else if (playerAction == 'stay') {
-            alert("Player stays on " + playerHand);
+        }
+        else if (playerAction == 'stay') {
+            alert("Player stays on " + playerHand + "\nPlayer hand: " + playerHand);
             break;
         }
     } while (playerHand <= 21)
-
+    alert("Dealers reveals: " + dealerHand + "\nPlayer's Hand :" + playerHand)
     if (playerHand < 22) {
-        let dealerHand = dealerNew();
-
         do {
             if (dealerHand > 17 && dealerHand <= 21) {
                 alert("Dealer stays on " + dealerHand);
                 break;
             } else if (dealerHand > 21) {
-                alert("Dealer busts")
+                alert("Dealer busts with " + dealerHand + "\nPlayer wins!");
                 break;
             } else if (dealerHand < 17) {
-                let newcard = draw()
-                alert("Dealer draws a " + cardType(newcard));
+                let newcard = draw();
                 dealerHand = dealerHand + cardValue(newcard);
-                alert("Dealer's new total is: " + dealerHand);
+                alert("Dealer draws a " + cardType(newcard) + "\nDealer's new total is: " + dealerHand + "\nPlayer's hand: " + playerHand);
             }
-        } while (dealerHand !== true)
-
-        console.log(dealerHand + " " + playerHand)
+        } while (dealerHand < 100)
     }
-    if (playerHand > dealerHand && playerHand <= 21) {
-        alert("Players " + playerHand + " beats dealers " + dealerHand);
+    if (playerHand > dealerHand && playerHand <= 21 && dealerHand < 21) {
+        alert("Players " + playerHand + " beats dealers " + dealerHand + "\nWinner Winner Chicken Dinner!");
         alert("Player wins lotsa money");
     } else if (playerHand <= 21 && dealerHand > 21) {
-        alert("Dealer busted, winner winner chicken dinner!")
+        alert("Winner Winner Chicken Dinner!")
     } else if (playerHand == dealerHand) {
         alert("Its a push. Both dealer and player have " + playerHand);
     } else {
-        alert("Better luck next time!");
+        alert("Dealer's " + dealerHand + " beats Player's " + playerHand + "\nluck next time!");
     }
     playGame = confirm("Would you like to play Blackjack?")
 
@@ -174,9 +179,8 @@ function dealerNew (){
     let total = 0;
     total = cardValue(firstCard) + cardValue(secondCard);
 
-    alert("Dealer's hand: " + cardType(firstCard) + " & " + cardType(secondCard));
-
-    alert("Dealer's hand current total: " + total);
+    alert("Dealer showing: " + cardType(firstCard));
+    // alert("Dealer's hand current total: " + total);
     return total;
 
 }
